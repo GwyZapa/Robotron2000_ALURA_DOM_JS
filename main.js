571e7238cbef5dfd64395c10ee00c3fd30da97e3
Guilherme Zapater <guizapa368@gmail.com>
2023-03-09 17:18:10 -0300

    commit 2

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const botao  = document.querySelector('#robotron'); 
-botao.addEventListener("click", (evento) => {
-    console.log("Cliquei no robô")
-    console.log(evento)
-});
+const braco = document.querySelector('#braco')
+const somar = document.querySelector('#somar');
+const subtrair = document.querySelector('#subtrair');
 
-function dizOi(nome){
-    console.log("Oi "+nome)
-    console.log("Bem Vindo ao Robotron 2000")
-    
+const controle = document.querySelectorAll(".controle-ajuste")
+
+
+controle.forEach((elemento)=>{
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+
+
+function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
-
-dizOi("Jorge");
